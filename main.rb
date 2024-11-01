4a0b4a84661c53d38780f438791b71d14bb71a9a
nabilhayet <nabil4457@yahoo.com>
2024-11-01 17:32:14 -0400

    added number of characters and user input if no filename provided

--- main.rb
+++ main.rb
@@ -28,9 +28,34 @@ while(input != 'Exit')
     else
       puts "File not found."
     end
+  elsif(input.match(/ccwc\s\w*.txt/))
+    text = input.split(" ").last
+    if(File.exist?(text))
+       GetLines(text)
+       GetWords(text)
+       GetChars(text)
+    else
+      puts "File not found."
+    end 
+elsif(input.match(/cat\s\w*.txt/) || input.match(/ccwc\s\-l/))
+  text = input.split(" ").last
+  if(text.match(/\.txt$/))
+    if(File.exist?(text))
+      GetLines(text)
+    else
+     puts "File not found."
+    end 
   else
-    puts "Invalid input. Please try again."
+    while(true)
+      puts "Please type something!"
+      user_input = gets.chomp
+      puts "Total number of line #{user_input.lines.count}" 
+      break
+    end 
   end 
+else
+  puts "Invalid input. Please try again."
+end 
   puts "Please enter your input"
   input = gets.chomp
 end 
